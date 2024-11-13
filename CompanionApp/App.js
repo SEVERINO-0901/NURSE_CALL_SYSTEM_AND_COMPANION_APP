c6063da3223888f02c258d6fc3a02ea83d1f89f6
Leonardo Severino <LeoSeverino0901@gmail.com>
2024-11-13 10:25:20 -0300

    Add files via upload

--- CompanionApp/App.js
+++ CompanionApp/App.js
@@ -1,12 +1,20 @@
+/*
+NOME DO PROJETO: CompanionApp                                                           
+AUTOR: Leonardo Severino - leoseverino0901@gmail.com
+DESCRIÇÃO:
+  Implementação em React Native de uma aplicação para interagir com o sistema de chamadas de enfermagem, permitindo ao usuário visualizar os dados da chamada para que este possa responder ao chamado do paciente.
+*/
+
+//Importação dos componentes
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import CallScreen from "./screens/CallScreen";
 
-const Stack = createNativeStackNavigator();
+const Stack = createNativeStackNavigator(); //Stack de navegação
 
 
 
-export default function App() {
+export default function App(){ //Função 'App'
   return (
     <NavigationContainer>
       <Stack.Navigator
--- CompanionApp/Server.js
+++ CompanionApp/Server.js
@@ -1,11 +1,11 @@
-// server.js
+//Servidor para receber os chamados
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const { default: axios } = require('axios');
 
 const app = express();
-const port = 3000;
+const port = 3000; //Porta do servidor
 
 app.use(cors()); // Permitir CORS
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -17,22 +17,22 @@ let calls = []; //Lista de chamadas recebidas
 //rota para receber dados do esp32
 app.post("/sendCall", (req, res) => {
   const { priority,  timestamp, pacient, serverMAC, clientMAC, lampIP } = req.body; //Registra os dados
-  if(!pacient || !priority || !timestamp || !serverMAC || !clientMAC || !lampIP){
-    return res.status(400).send("Missing data");
+  if(!pacient || !priority || !timestamp || !serverMAC || !clientMAC || !lampIP){ //Se tiver erro
+    return res.status(400).send("Missing data"); //Retorna erro e informa
   }
-  else{
+  else{ //Senão
     console.log("Data Received: ");
     console.log(priority,  timestamp, pacient, serverMAC, clientMAC, lampIP);
     calls.push({priority, timestamp, pacient, serverMAC, clientMAC, lampIP}); //Adiciona a chamada ao final da fila
-    res.json({message: "Data received sucessfuly!"}); // Responder para o ESP32 que os dados foram recebidos
+    res.json({message: "Data received sucessfuly!"}); //Responder para o ESP32 que os dados foram recebidos
   }
 });
 //rota para receber OFF
 app.post("/sendOff", (req, res) => {
-  const pacient = req.body.pacient;
-  const lampIP = req.body.lampIP;
+  const pacient = req.body.pacient; //Paciente
+  const lampIP = req.body.lampIP; //Ip do esp32 da lampada
 
-  if (pacient && lampIP) {
+  if(pacient && lampIP){ //Se os dados estiverem corretos
     console.log("Removing call for pacient:", pacient);
     // Envia uma requisição POST para o ESP32 com o parametro 'pacient'
     axios.post(`http://${lampIP}/receiveOff`, `pacient=${encodeURIComponent(pacient)}`, {
@@ -40,8 +40,9 @@ app.post("/sendOff", (req, res) => {
     })
     .then(response => res.status(200).send("Call removed"))
     .catch(error => res.status(500).send("Error removing call"));
-  } else {
-    res.status(400).send("No pacient or lampIP provided");
+  }
+  else{ //Senão
+    res.status(400).send("No pacient or lampIP provided"); //Informa que houve um erro
   }
 });
 
@@ -52,11 +53,12 @@ app.get("/getCalls", (req, res) => {
     const call = calls.shift(); //Remove a chamada mais antiga
     res.json(call); //Retorna a chamada mais antiga
   }
-  else{
-    res.status(404).send("N/A");
+  else{ //Senão
+    res.status(404).send("N/A"); //Informa que houve um erro
   }
 });
 
+//Mantém o servidor rodando
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
